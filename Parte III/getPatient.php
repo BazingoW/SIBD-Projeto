82abe39b0dee6d9537470aca59ff4df451288a6a
catarinaacfa <catariinaacfa@gmail.com>
2017-11-11 20:39:41 +0000

    getPatient added

---
+++ Parte III/getPatient.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Schedule Appointment</title>
+	</head>
+	<body>
+		<h1><strong>Welcome to the Healthcare Centre</strong></h1>
+		<hr/>
+		<form action=""> /* Falta pôr aqui o script php que avalia se o paciente existe */
+ 			<p>Patient Name:
+				<input type ="text" name="name"/>
+				<input type="submit" name="Search"/>
+			</p>
+		</form>
+	</body>
+</html>
---
+++ Parte III/scheduleAppointment.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Schedule Appointment</title>
+	</head>
+	<body>
+		<h1><strong>Welcome to the Healthcare Centre</strong></h1>
+		<hr/>
+
+
+	</body>
+</html>
