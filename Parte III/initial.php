bf07201fb07a3c8631d561d6030f7e2eba2b7deb
catarinaacfa <catariinaacfa@gmail.com>
2017-11-23 02:17:16 +0000

    1, 2 and 3

---
+++ Parte III/initial.php
@@ -0,0 +1,15 @@
+<html>
+	<body>
+		<h1><strong><font color= '#66CC00'>Welcome to the Healthcare Centre</font></strong></h1>
+		<hr/>
+		<form action="getPatient.php" method="post">
+ 			<p>Search for/Insert a patient: <input type="submit" value="Search"/></p>
+		</form>
+		<form action="createStudy.php" method="post">
+ 			<p>Create a study: <input type="submit" value="Search"/></p>
+		</form>
+		<form action="addRegion.php" method="post">
+ 			<p>Add a region to an element: <input type="submit" value="Add Region"/></p>
+		</form>
+	</body>
+</html>
